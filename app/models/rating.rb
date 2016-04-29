a1db506937acc6173f0d1d1efef2113ab2eb2389
jnf <jeremy.flores@gmail.com>
2016-04-29 14:54:41 -0700

    use an after_save hook in Rating to update the denormalized rating stuffs

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -10,4 +10,26 @@ class Rating < ActiveRecord::Base
     greater_than_or_equal_to: 0
   }
 
+  after_save :update_user_rating_data
+
+  def z_score
+    return 0 unless user.rating_standard_deviation
+    (score - user.rating_average) / user.rating_standard_deviation
+  end
+
+  private
+
+  def update_user_rating_data
+    # get updated average & std dev for the user's scores
+    scores = Rating.select("stddev(score) as std, avg(score) as avg").group(:user_id).where(user: user)[0]
+    
+    # store those calculated values in the denormalized columns
+    user.update(rating_average: scores.avg, rating_standard_deviation: scores.std)
+
+    # calculate the associated Proposal's new z_score average
+    z_avg = proposal.ratings.to_a.sum(&:z_score) / proposal.ratings.length
+
+    # store the calculated z_avg in the denormalized column
+    proposal.update(z_average: z_avg)
+  end
 end
